9223a61b5bdb8a7a74f1936a8caca77a9e869632
hema <hemamounikakotte@gmail.com>
2018-10-24 15:37:21 -0400

    unit test1: test for string datatype

---
+++ tests/Unit/UserTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\User;
+
+class UserTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $user = User::inRandomOrder()->first();
+
+        $this->assertInternalType('int',$user->id);
+        $this->assertInstanceOf('App\User',$user);
+    }
+
+    public function testExample3()
+    {
+        $user = User::inRandomOrder()->first();
+
+        $this->assertInternalType('string',$user->name);
+
+    }
+}
